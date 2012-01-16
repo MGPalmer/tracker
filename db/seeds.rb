a02a38cb31a7dc94b5397ccad055f32c6f5457f2
Martin Tepper <mgpalmer@monogreen.de>
2012-01-16 17:13:29 +0100

    Add setup notes and seeds for the next dev to start with this project...

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+mail = "mail@example.com"
+pass = "password"
+AdminUser.create!(:email => mail, :password => pass, :password_confirmation => pass)
+puts "Admin user with '#{mail}'/'#{pass}' created"
+c = Campaign.create!(:name => "Example Campaign", :ref => "example")
+s = Script.create!(:content => '<p>This is a tracking device (Your setup is working!):</p><img src="https://encrypted-tbn3.google.com/images?q=tbn:ANd9GcQXIwpUArUea62IYCWcfA4iFYEybJ3mW5r06QIgjZXsBlebEkreNLhP8z3jbw"/>')
+c.scripts << s
+puts "Created example campaign/script (go to localhost:3000/?ref=example)"
