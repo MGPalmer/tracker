bb08c6946db0b60dad167202c51377a5b51cfe60
Martin Tepper <mgpalmer@monogreen.de>
2012-01-17 14:52:08 +0100

    Only routes we need

--- config/routes.rb
+++ config/routes.rb
@@ -50,8 +50,8 @@ Tracker::Application.routes.draw do
   #     resources :products
   #   end
 
-  resources :examples
-  resources :pixels
+  resources :examples, :only => :show
+  resources :pixels, :only => :index
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'examples#show'
